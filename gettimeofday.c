fa96cbe263a8b0a249165ee85d81051c797739fa
Gianni7315 <121824708+Gianni7315@users.noreply.github.com>
2024-05-16 10:41:04 +0200

    Add files via upload

---
+++ gettimeofday.c
@@ -0,0 +1,7 @@
+#include <sys/time.h>
+
+int _gettimeofday( struct timeval *tv, void *tzvp )
+{
+    // you can add code here there many example in google search. 
+    return 0;  // return non-zero for error
+} // end _gettimeofday()
